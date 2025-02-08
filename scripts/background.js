c08391d8b3c7688da6494631185bf7812d811bed
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-02-08 14:51:17 -0800

    made styling consistent

--- scripts/background.js
+++ scripts/background.js
@@ -12,36 +12,36 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 async function fetchProfData(sendResponse, profName) {
-  try {
-    const encodedName = encodeURIComponent(profName);
-    const url = `https://rmp-retriever.onrender.com/professor?name=${encodedName}&id=U2Nob29sLTE2MDA=`
+    try {
+        const encodedName = encodeURIComponent(profName);
+        const url = `https://rmp-retriever.onrender.com/professor?name=${encodedName}&id=U2Nob29sLTE2MDA=`
 
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        sendResponse({ success: true, data });
+    } catch (error) {
+        console.error("API Fetch Error:", error);
+        sendResponse({ success: false, error: error.message });
     }
-
-    const data = await response.json();
-    sendResponse({ success: true, data });
-  } catch (error) {
-    console.error("API Fetch Error:", error);
-    sendResponse({ success: false, error: error.message });
-  }
 }
 
 async function fetchRatingData(sendResponse, profId) {
-  try {
-    const url = `https://rmp-retriever.onrender.com/ratings?id=${profId}`
+    try {
+        const url = `https://rmp-retriever.onrender.com/ratings?id=${profId}`
 
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        sendResponse({ success: true, data });
+    } catch (error) {
+        console.error("API Fetch Error:", error);
+        sendResponse({ success: false, error: error.message });
     }
-
-    const data = await response.json();
-    sendResponse({ success: true, data });
-  } catch (error) {
-    console.error("API Fetch Error:", error);
-    sendResponse({ success: false, error: error.message });
-  }
 }
